348f7926b38dd9f7f8f7e59afd10c4b8576e57f7
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-11-13 14:19:18 +0100

    :rocket: add topbar desktop navigation experience

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import Layout from "~/components/primitives/composing/Layout.vue";
+import TopBar from "~/components/shared/top-bar/TopBar.vue";
 </script>
 
 <template>
-  <Layout>
-    <NuxtPage />
+  <Layout class="flex flex-col gap-4 min-h-dvh p-6 pt-0">
+    <TopBar />
+
+    <div class="flex-1">
+      <NuxtPage />
+    </div>
   </Layout>
 </template>
