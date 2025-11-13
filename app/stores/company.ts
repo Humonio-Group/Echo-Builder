dae6f94e813b66752ad4d770712860153f7f61bb
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-11-13 14:18:54 +0100

    :rocket: add company store

---
+++ app/stores/company.ts
@@ -0,0 +1,35 @@
+import type { CompanyState } from "~~/shares/types/stores/company";
+import type { CompanyBrandingMeta } from "~~/shares/types/company";
+
+export const useCompanyStore = defineStore("company", {
+  state: (): CompanyState => ({
+    name: null,
+    icon: null,
+    logo: null,
+    colors: null,
+  }),
+  getters: {
+    isLoaded: state => !!state.name || !!state.icon || !!state.logo || !!state.colors,
+  },
+  actions: {
+    async loadBranding() {
+      try {
+        const branding = await $fetch<CompanyBrandingMeta>(useApi2Url("/branding", "v1"), {
+          headers: useHeaders(),
+          query: useParams(),
+        });
+
+        this.name = branding.meta.company;
+        this.icon = branding.meta.icon;
+        this.logo = branding.meta.logo;
+        this.colors = {
+          first: branding.meta.colors.firstGradient,
+          second: branding.meta.colors.secondGradient,
+        };
+      }
+      catch {
+        console.error("Failed to load branding");
+      }
+    },
+  },
+});
