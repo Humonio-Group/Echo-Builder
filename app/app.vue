3b7b3471c3680fd2cb81376563dccc8acc682002
loicmaes <97891718+loicmaes@users.noreply.github.com>
2025-11-13 14:20:53 +0100

    :rocket: handle not saved changes leave behavior

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,52 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted } from "vue";
+import "vue-sonner/style.css";
+
+const { enableConfirmation, disableConfirmation, shouldConfirmBeforeUnload } = useBeforeUnload();
+
+const builderStore = useBuilderStore();
+builderStore.$subscribe((x) => {
+  console.log(x, builderStore.touched);
+  if (x.events.key === "touched") return;
+  builderStore.touch();
+});
+const { touched } = storeToRefs(builderStore);
+watch(touched, (val) => {
+  if (val) enableConfirmation();
+  else disableConfirmation();
+});
+
+const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+  if (shouldConfirmBeforeUnload.value) {
+    event.preventDefault();
+    event.returnValue = "";
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("beforeunload", handleBeforeUnload);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("beforeunload", handleBeforeUnload);
+});
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtLoadingIndicator />
-    <NuxtLayout />
+
+    <Toaster
+      :theme="$colorMode.preference"
+      close-button
+      close-button-position="top-right"
+      rich-colors
+      position="top-right"
+    />
+
+    <TooltipProvider>
+      <NuxtLayout />
+    </TooltipProvider>
   </div>
 </template>
